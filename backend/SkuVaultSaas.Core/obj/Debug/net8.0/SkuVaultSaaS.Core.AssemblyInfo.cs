edbdd7a3e1aa586bdfda2c85a7d69d3614a05d49
Dave Baumann <dcbaumann@hotmail.com>
2025-11-18 13:13:58 -0600

    Force .NET 8.0: remove nixpacks.toml and use global.json to specify SDK version

--- backend/SkuVaultSaas.Core/obj/Debug/net8.0/SkuVaultSaaS.Core.AssemblyInfo.cs
+++ backend/SkuVaultSaas.Core/obj/Debug/net8.0/SkuVaultSaaS.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SkuVaultSaaS.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4538a54298dc029f9277734da53111831741def")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6561d37d9e42d9420a22a17f8e9738bdfba4b6d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkuVaultSaaS.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SkuVaultSaaS.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
